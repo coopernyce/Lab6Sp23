9def9dda1a876fa2f2fb70d239e132dfb56fdc73
coopernyce <cooper.nyce@ufl.edu>
2023-03-09 13:59:54 -0500

    Wrote my encode function

---
+++ main.py
@@ -0,0 +1,28 @@
+def encode(phrase):
+    output = ''
+    for i in phrase:
+        new_value = (int(i)+ 3) % 10
+        output += str(new_value)
+    return output
+
+def decode(phrase):
+    pass
+def main():
+    # looping menu
+    option = '1'
+    phrase = ''
+    while option != '0':
+        # print menu
+        print('0. Exit')
+        print('1. Enter a new phrase')
+        print('2. Print encoded phrase')
+        print('3. Print decoded phrase')
+        option = input('Enter an option: ')
+
+        if option == '1':
+            phrase = input('Enter your phrase: ')
+        elif option == '2':
+            print('Encoded phrase is', encode(phrase))
+        elif option == '3':
+            print('Decoded phrase is', decode(phrase))
+
